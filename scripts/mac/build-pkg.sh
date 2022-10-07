d39b335551e74cdfd233e757ecfb96a00918befe
Ben Rockwood <benr@cuddletech.com>
2022-10-06 17:48:08 -0700

    Update build-pkg script for cnquery rather than mondoo
    Signed-off-by: Ben Rockwood <benr@cuddletech.com>

--- scripts/mac/build-pkg.sh
+++ scripts/mac/build-pkg.sh
@@ -29,8 +29,8 @@ echo "Packaging Release ${VERSION}"
 ###############################################################################################################
 echo "Creating Universal Binary...."    
 cd dist/macos/
-/usr/bin/lipo -create -output mondoo mac_darwin_amd64/mondoo mac_darwin_arm64/mondoo
-if [ ! -f mondoo ]; then
+/usr/bin/lipo -create -output cnquery mac_darwin_amd64/cnquery mac_darwin_arm64/cnquery
+if [ ! -f cnquery ]; then
   echo "ERROR: Failure in creating universal binary, please investigate lipo create in $PWD"
   exit 2
 else
@@ -41,19 +41,19 @@ fi
 
 ###############################################################################################################
 echo "Signing Universal Binary...."    
-codesign -s "Developer ID Application: Mondoo, Inc. (W2KUBWKG84)" -f -v --timestamp --options runtime ${BLDDIR}/scripts/pkg/mac/packager/application/bin/mondoo
+codesign -s "Developer ID Application: Mondoo, Inc. (W2KUBWKG84)" -f -v --timestamp --options runtime ${BLDDIR}/scripts/mac/packager/application/bin/cnquery
 
 
 
 ###############################################################################################################
 echo "Building Package...."            
-cd ${BLDDIR}/scripts/pkg/mac/
+cd ${BLDDIR}/scripts/mac/
 bash packager/build-package.sh Mondoo ${VERSION} 
 
-PKG=packager/target/pkg/Mondoo-macos-universal-${VERSION}.pkg
+PKG=packager/target/pkg/cnquery-macos-universal-${VERSION}.pkg
 if [ -f $PKG ]; then
   mv $PKG .
-  echo "SUCCESS! Your package is ready to rock, hot and fresh: Mondoo-macos-universal-${VERSION}.pkg"
+  echo "SUCCESS! Your package is ready to rock, hot and fresh: cnquery-macos-universal-${VERSION}.pkg"
 else 
   echo "ERROR: Something went wrong building the package... time to debug. :(" 
   exit 2
@@ -62,8 +62,8 @@ fi
 
 ###############################################################################################################
 echo "Signing Package..."               
-productsign --sign "Developer ID Installer: Mondoo, Inc. (W2KUBWKG84)" Mondoo-macos-universal-${VERSION}.pkg mondoo_${VERSION}_darwin_universal.pkg
-if [ ! -f mondoo_${VERSION}_darwin_universal.pkg ]; then
+productsign --sign "Developer ID Installer: Mondoo, Inc. (W2KUBWKG84)"cnquery-macos-universal-${VERSION}.pkg cnquery_${VERSION}_darwin_universal.pkg
+if [ ! -f cnquery_${VERSION}_darwin_universal.pkg ]; then
   echo "ERROR: Signing failed. :("
   exit 2
 fi
@@ -71,7 +71,7 @@ fi
 
 ###############################################################################################################
 echo "Notorizing Package with user ${APPLE_ID_USERNAME}...."           
-cat notorize.hcl.tmpl | sed s/PACKAGE_PATH/mondoo_${VERSION}_darwin_universal.pkg/ \
+cat notorize.hcl.tmpl | sed s/PACKAGE_PATH/cnquery_${VERSION}_darwin_universal.pkg/ \
                       | sed s/APPLE_ID_USERNAME/${APPLE_ID_USERNAME}/ \
                       | sed s/APPLE_ID_PASSWORD/${APPLE_ID_PASSWORD}/  > notorize.hcl 
 gon -log-level=info notorize.hcl
@@ -79,8 +79,8 @@ gon -log-level=info notorize.hcl
 
 ###############################################################################################################
 echo "Copying package to dist...."      
-shasum -a 256 mondoo_${VERSION}_darwin_universal.pkg >> ${BLDDIR}/dist/macos/checksums.macos.txt 
-cp mondoo_${VERSION}_darwin_universal.pkg ${BLDDIR}/dist/macos/
+shasum -a 256 cnquery_${VERSION}_darwin_universal.pkg >> ${BLDDIR}/dist/macos/checksums.macos.txt 
+cp cnquery_${VERSION}_darwin_universal.pkg ${BLDDIR}/dist/macos/
 
 ls -l ${BLDDIR}/dist/macos/${PKG_NAME}
 echo "All done here. Good bye."         
